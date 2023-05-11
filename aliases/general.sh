8e09225484942b0a908ca77d9ea1d8a41db7be31
Joanne Truong <truong.j@husky.neu.edu>
2023-05-11 14:02:25 -0700

    Update general.sh

--- aliases/general.sh
+++ aliases/general.sh
@@ -1,10 +1,10 @@
 # Remove hostname from command line prompt prefix, just use username.
 # Username will appear cyan in color. Conda env prefix recovered by env re-activation.
-#PS1='\[\e[1;36m\]\u:\[\e[1;34m\]\w\[\e[0m\]$ '
-#if [ ! -z "$CONDA_DEFAULT_ENV" ]
-#then
-#      conda activate $CONDA_DEFAULT_ENV
-#fi
+PS1='\[\e[1;36m\]\u:\[\e[1;34m\]\w\[\e[0m\]$ '
+if [ ! -z "$CONDA_DEFAULT_ENV" ]
+then
+      conda activate $CONDA_DEFAULT_ENV
+fi
 
 # General
 alias sba='source ~/.bash_aliases'
