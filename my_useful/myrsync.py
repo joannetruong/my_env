d9d52d6b5bbd1d725eaae4223552d93e320aba80
Joanne Truong <joanne_truong2@joanne-4.local>
2023-05-16 16:35:51 -0700

    add hostname to myrsync

--- my_useful/myrsync.py
+++ my_useful/myrsync.py
@@ -5,8 +5,11 @@ import os
 copy_cmd = (
     "rsync -rav --progress -e "
     "'ssh -T -c chacha20-poly1305@openssh.com -o Compression=no -x' "
-    "{exclude} {src} {dst}"
+    "{exclude} {remote_host}:{src} {dst}"
 )
+
+
+REMOTE_HOST = os.environ["DEFAULT_REMOTE_HOST"]
 
 
 def main(src, dst, exclude):
@@ -15,7 +18,7 @@ def main(src, dst, exclude):
     dst = dst.replace(os.environ["HOME"], "~")
     if exclude != "":
         exclude = "--exclude=" + exclude.replace(",", " --exclude=")
-    cmd = copy_cmd.format(exclude=exclude, src=src, dst=dst)
+    cmd = copy_cmd.format(exclude=exclude, remote_host=REMOTE_HOST, src=src, dst=dst)
     print(f"Executing:\n{cmd}")
     subprocess.check_call(cmd, shell=True)
 
